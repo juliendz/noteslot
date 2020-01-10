5c1765faea809d7cc716a3cf7063baf0f3cfbf16
Julien Dcruz <juliendcruz@gmail.com>
2020-01-10 18:18:48 +0530

    titlebar and taskbar icon

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Noteslot is sticky note taking desktop application inspired by Tomboy Notes. It aims to make it cross platform and extend the feature sets available in Tomboy Notes.
+Noteslot is a sticky notes application for the desktop inspired by Tomboy Notes. It aims to make it cross platform and extend the feature set available in Tomboy Notes.
 
 Noteslot has no relation/affiliation to Tomboy notes at all besides being inspired by it design wise.
--- src/noteslot/__init__.py
+++ src/noteslot/__init__.py
@@ -1,8 +1,10 @@
 
 from PySide2 import QtCore
 from PySide2.QtWidgets import QApplication
+from PySide2.QtGui import QWindow, QIcon, QPixmap
 from noteslot.constants import HUMAN_APP_NAME
 from noteslot.init import init_app_data_dir, init_app_db
+from noteslot import noteslot_rc
 
 
 def run(argv):
@@ -22,6 +24,7 @@ def run(argv):
     app = QApplication(argv)
     app.setApplicationName(HUMAN_APP_NAME)
     w = MainWindow()
+    w.setWindowIcon(QIcon(QPixmap(':/icons/resources/icons/noteslot.png')))
 
     # with open('styles/Aqua.qss', 'r') as stylesheet:
     #     qss = stylesheet.read()
