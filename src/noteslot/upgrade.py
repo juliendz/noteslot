b89d0f97ba095b4024c024a0293168da703e0f9e
Julien Dcruz <juliendcruz@gmail.com>
2020-01-18 12:29:46 +0530

    Update in-db version to updated in-code version at startup

--- src/noteslot/upgrade.py
+++ src/noteslot/upgrade.py
@@ -9,30 +9,30 @@ Application Version Upgrade module
 from packaging.version import Version
 from noteslot.log import LOGGER
 from noteslot.db import dbmgr
+from noteslot.constants import APP_VERSION
 
 
 def upgrade_from_previous_versions(cur_version, db_path):
     db = dbmgr(db_path)
     db.connect()
 
-    if cur_version < Version('0.9.1'):
-        LOGGER.info('Upgrading from Version:%s to Version:%s' %
-                    (cur_version, '0.8.1'))
+    # if cur_version < Version('1.0.0-beta'):
+    #     LOGGER.info('Upgrading from Version:%s to Version:%s' %
+    #                 (cur_version, '1.0.0-beta'))
+    #     queries = [
+    #         'ALTER TABLE notes ADD mtime TEXT;'
+    #     ]
+    #     for query in queries:
+    #         db.run_query(query)
 
-        query = "UPDATE settings SET value=? WHERE key='VERSION'"
-        db.run_query(query, ('0.9.1',))
+    #     query = "UPDATE settings SET value=? WHERE key='VERSION'"
+    #     db.run_query(query, ('1.0.0-beta',))
 
-    if cur_version < Version('1.0.0-beta'):
-        LOGGER.info('Upgrading from Version:%s to Version:%s' %
-                    (cur_version, '1.0.0-beta'))
-        queries = [
-            'ALTER TABLE notes ADD mtime TEXT;'
-        ]
-        for query in queries:
-            db.run_query(query)
-
-        query = "UPDATE settings SET value=? WHERE key='VERSION'"
-        db.run_query(query, ('1.0.0-beta',))
+    #
+    # Finally, update the in-database version information.
+    #
+    query = "UPDATE settings SET value=? WHERE key='VERSION'"
+    db.run_query(query, (APP_VERSION.__str__(),))
 
     db.disconnect()
     return
